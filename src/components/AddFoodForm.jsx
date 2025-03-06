3a87debabfd8b19474bec80d65cbbc9c3ff29b0d
Juan Marin <jmo129@gmail.com>
2025-03-06 18:55:26 +0100

    arreglar añadir producto

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,67 @@
 // Your code here
+
+import React, { useState } from 'react'
+
+
+
+function AddFoodForm(props) {
+
+    const [nuevaComida, setNuevaComida] = useState({
+        name: "",
+        img: "",
+        calories: 0,
+        servings: 0
+    })
+
+
+    const rellenarValores = ((event) => {
+        let name = event.target.name;
+        console.log("Esto es event" + event)
+        let clone = {...nuevaComida}
+        clone[name] = event.target.value
+        console.log("Esto es la nueva comida " + clone)
+        setNuevaComida(clone)
+    })
+
+    const pulsarCrearComida = (event) => {
+        event.preventDefault();
+
+        const clone = [...props.allFood]
+        clone.unshift(nuevaComida)
+        props.setAllFood(clone)
+
+        setNuevaComida({
+            name: "",
+            img: "",
+            calories: 0,
+            servings: 0
+        })
+    }
+
+
+
+  return (
+    <div>
+        <form onSubmit={pulsarCrearComida}  style={{display: "flex", justifyContent: "center"}}>
+            <div className='add-food'>
+                <h2>Añadir Comida</h2>
+                <label>Name: </label>
+                <input type="text" onChange={rellenarValores} />
+                <label>Image: </label>
+                <input type="text" onChange={rellenarValores} />
+                <label>Calories: </label>
+                <input type="number" onChange={rellenarValores} />
+                <label>Servings: </label>
+                <input type="number" onChange={rellenarValores} />
+
+                <button className="btnCSS" type="submit">Create</button>
+            </div>
+        </form>
+
+
+
+    </div>
+  )
+}
+
+export default AddFoodForm
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,39 @@
 // Your code here
+
+import React from 'react'
+
+function FoodBox(props) {
+
+    const totalCalories = props.food.servings * props.food.calories;
+
+    //console.log(props.indice)
+
+
+    // Para eliminar un elemento, hacemos un clon de la lista con slice y con splice lo quitamos. El indice nos dice cúal elemento es el que debemos eliminar.
+    // Con setAllFood actualizamos el Estado de la lista.
+    const pulsarDelete = () => {
+        let clone = props.allFood.slice(0);
+        clone.splice(props.indice,1);
+        props.setAllFood(clone);
+    }
+
+    
+  return (
+    <div style={{border: "0.5px solid lightgrey", margin: "10px"}}>
+    <h2>{props.food.name}</h2>
+  
+    <img src={props.food.image} alt="foto-comida" width="400px" />
+  
+    <p>Calories: {props.food.calories}</p>
+    <p>Servings {props.food.servings}</p>
+  
+    <p>
+      <b>Total Calories: {totalCalories} </b> kcal
+    </p>
+  
+    <button onClick={pulsarDelete} style={{border: "0.5px solid black", backgroundColor: "lightgray", marginBottom: "5px"}}>Delete</button>
+  </div>
+  )
+}
+
+export default FoodBox
